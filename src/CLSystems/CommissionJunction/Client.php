ff69cac7c57b585812dd2076420935b269aa6881
orbex <jeroen@clsystems.nl>
2021-04-29 08:11:12 +0200

    Add responseToRecords method

--- src/CLSystems/CommissionJunction/Client.php
+++ src/CLSystems/CommissionJunction/Client.php
@@ -3,7 +3,8 @@
 namespace CLSystems\CommissionJunction;
 
 use GuzzleHttp\Client as GuzzleClient;
-use CLSystems\CommissionJunction\Plugin\AuthTokenPlugin;
+use GuzzleHttp\Psr7\Response;
+use SimpleXMLElement;
 
 /**
  * Class Client
@@ -12,21 +13,78 @@ use CLSystems\CommissionJunction\Plugin\AuthTokenPlugin;
  */
 class Client extends GuzzleClient
 {
-	protected $base_url = "https://{subdomain}.api.cj.com/{version}/";
-
-	public function __construct($auth_token, $subdomain, $version = 'v3')
+	/**
+	 * Client constructor.
+	 *
+	 * @param string $authToken
+	 * @param string $subdomain
+	 * @param string $version
+	 */
+	public function __construct(string $authToken, string $subdomain, $version = 'v3')
 	{
-
+		$baseUrl = 'https://' . $subdomain . '.api.cj.com/' . $version . '/';
 		parent::__construct([
-			'base_url' => [
-				$this->base_url, [
-					'subdomain' => $subdomain,
-					'version' => $version
-				]
-			]
+			'headers' => [
+				'authorization' => 'Bearer ' . $authToken,
+			],
+			'base_uri' => $baseUrl,
+			'verify' => false,
+			'options' => [],
 		]);
-
-		$this->getEmitter()->attach(new AuthTokenPlugin($auth_token));
-
 	}
+
+	/**
+	 * @param Response $httpResponse
+	 * @param $xmlRecordTag
+	 * @return array
+	 */
+	protected function responseToRecords(Response $httpResponse, $xmlRecordTag) : array
+	{
+		// parse response body
+		$xml = simplexml_load_string($httpResponse->getBody()->getContents());
+		if (false === $xml)
+		{
+			foreach (libxml_get_errors() as $error)
+			{
+				echo "\t", $error->message . PHP_EOL;
+			}
+		}
+		$records = [];
+		if ($xml instanceof SimpleXMLElement)
+		{
+			// convert xml to an array
+			$arr = @json_decode(@json_encode($xml), true);
+			if (isset($arr[$xmlRecordTag]))
+			{
+				$records = $arr[$xmlRecordTag];
+			}
+			// normalize the result array if we have a single result
+			if (is_array($arr) && !array_key_exists(0, $records))
+			{
+				$records = [$arr];
+			}
+
+			if (!is_array($records))
+			{
+				$records = [$records];
+			}
+
+			if (1 === count($records))
+			{
+				$records = reset($records);
+			}
+		}
+		else
+		{
+			echo "CJ - FALSE ELSE" . PHP_EOL;
+			foreach (libxml_get_errors() as $error)
+			{
+				echo "\t", $error->message . PHP_EOL;
+			}
+		}
+
+		return $records;
+	}
+
+
 }
